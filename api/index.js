7f3e0c224f057c27bacaae4192095a483203c0a6
Jay Zadafiya <82803534+jayzadafiya@users.noreply.github.com>
2023-09-01 10:56:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { db } = require("./config");
 const cors = require("cors");
 const app = express();
 app.use(cors({
-    origin: ["https://todo-frontend.vercel.app/"],
+    origin: ["https://todo-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DETELE"],
     credentials: true
 }));
