53ad9b21266ccc73b49382ccbeb62491794e9917
Jay Zadafiya <82803534+jayzadafiya@users.noreply.github.com>
2023-09-01 11:41:46 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   
   const fetchTodos = async () => {
     try {
-      const response = await axios.get('https://todo-opal-ten.vercel.app');
+      const response = await axios.get('https://todo-opal-ten.vercel.app/get');
       const todos = response?.data;
       const addedTodos = todos.filter(todo => todo?.type === 'Todo');
       const processTodos = todos.filter(todo => todo?.type === 'Doing');
